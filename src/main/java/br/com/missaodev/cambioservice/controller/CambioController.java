fd9e08aec469582c5c16cf6fa38fe44d82988ade
nayron.ferreira <May@211021>
2022-01-11 19:14:05 -0300

    Adcionando OpenAPI UI (swagger) e Spring Doc. Adcionado as 2 dependencias necessárias, Pacote Configuration e Anotações no COntroller.

--- src/main/java/br/com/missaodev/cambioservice/controller/CambioController.java
+++ src/main/java/br/com/missaodev/cambioservice/controller/CambioController.java
@@ -2,6 +2,8 @@ package br.com.missaodev.cambioservice.controller;
 
 import br.com.missaodev.cambioservice.model.Cambio;
 import br.com.missaodev.cambioservice.repository.CambioRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
-
+@Tag(name = "Microservice Cambio endpoint")
 @RestController
 @RequestMapping ("cambio-service")
 public class CambioController {
@@ -21,6 +23,7 @@ public class CambioController {
 	@Autowired
 	private CambioRepository repository;
 
+	@Operation(summary = "Converte o valor passado como parametro de/para as moedas também passadas por parametro.")
 	@GetMapping (value = "/{amount}/{from}/{to}")
 	public Cambio getCambio(
 			@PathVariable ("amount")BigDecimal amount,
